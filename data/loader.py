4e6e66e07c94c7c224a4295c602b6ccdd7c9c246
Samruddha Dinde <samruddha.dinde@gmail.com>
2026-04-16 01:32:10 +1000

    Loading MEDQA data

---
+++ data/loader.py
@@ -0,0 +1,24 @@
+from datasets import load_dataset
+import os
+
+
+def load_medqa(split: str="test", n_samples: int = 10):
+    ds = load_dataset(
+        "bigbio/med_qa",
+        "med_qa_en_source",
+        trust_remote_code=True,
+        token =os.getenv("HF_TOKEN")
+    )
+
+    data = ds[split].select(range(n_samples))
+    return data
+
+def format_question(example: dict) -> str:
+    options_str = "\n".join([f"{k}:{v}" for k, v in example["options"].items()])
+    return f"""Question: {example["question"]} Options: {options_str}"""
+
+def get_ground_truth(example:dict)-> str:
+    return example["answer_idx"]
+
+
+
